198c67bb83d1104ab73596236f6a025707607a3d
soam1 <soamakash9@gmail.com>
2023-07-18 17:20:05 +0530

    designed home, search fragment ui

--- app/src/main/java/com/akashsoam/instagramcloneapp/MainActivity.kt
+++ app/src/main/java/com/akashsoam/instagramcloneapp/MainActivity.kt
@@ -1,17 +1,72 @@
 package com.akashsoam.instagramcloneapp
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.fragment.app.Fragment
 import com.akashsoam.instagramcloneapp.databinding.ActivityMainBinding
+import com.akashsoam.instagramcloneapp.fragments.HomeFragment
+import com.akashsoam.instagramcloneapp.fragments.NotificationsFragment
+import com.akashsoam.instagramcloneapp.fragments.ProfileFragment
+import com.akashsoam.instagramcloneapp.fragments.SearchFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    internal var selectedFragment: Fragment? = null
+
+//    private lateinit var textView: TextView
+
+    private val onNavigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+//                    textView.text = "Home"
+//                moveToFragment(HomeFragment())
+//                    return@OnNavigationItemSelectedListener true
+                    selectedFragment = HomeFragment()
+                }
+
+                R.id.nav_search -> {
+//                    textView.setText("Search")
+//                moveToFragment(SearchFragment())
+//                    return@OnNavigationItemSelectedListener true
+                    selectedFragment = SearchFragment()
+
+                }
+
+                R.id.nav_add_post -> {
+//                    textView.setText("Add post")
+                    item.isChecked = false
+//                startActivity(Intent(this@MainActivity, AddPostActivity::class.java))
+                    return@OnNavigationItemSelectedListener true
+//                    selectedFragment = HomeFragment()
+
+                }
+
+                R.id.nav_notifications -> {
+//                    textView.setText("Notifications")
+//                moveToFragment(NotificationsFragment())
+//                    return@OnNavigationItemSelectedListener true
+                    selectedFragment = NotificationsFragment()
+
+                }
+
+                R.id.nav_profile -> {
+//                    textView.setText("Profile")
+//                moveToFragment(ProfileFragment())
+//                    return@OnNavigationItemSelectedListener true
+                    selectedFragment = ProfileFragment()
+                }
+            }
+
+            if (selectedFragment != null) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, selectedFragment!!)
+                    .commit()
+            }
+            false
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,17 +74,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+//        textView = findViewById(R.id.message)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HomeFragment())
+            .commit()
     }
 }
